fbb8ae63f8b8bd3fd5cb10ebcd5ff758c978fd54
NimaBoscarino <nima.boscarino@gmail.com>
2019-01-08 20:18:15 -0800

    pick random restaurant

--- api/index.js
+++ api/index.js
@@ -9,7 +9,11 @@ const setTimeoutPromise = util.promisify(setTimeout);
   const restaurants = await page.$$eval('a[data-restaurant-open="1"] .truncated-name', divs => {
     return divs.map(div => div.innerHTML)
   })
-  restaurants.forEach(console.log)
+  // restaurants.forEach(console.log)
+
+  const restaurantIndex = Math.floor(Math.random() * restaurants.length) + 1  
+
+  console.log(restaurants[restaurantIndex])
 
   await browser.close()
 })()
