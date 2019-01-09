c02bbc158f58697fa2126396717008ebace8356f
NimaBoscarino <nima.boscarino@gmail.com>
2019-01-08 20:09:55 -0800

    only retrieve open restaurants

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const setTimeoutPromise = util.promisify(setTimeout);
   const page = await browser.newPage()
   await page.goto('https://www.skipthedishes.com/vancouver/restaurants')
 
-  const restaurants = await page.$$eval('.truncated-name', divs => {
+  const restaurants = await page.$$eval('a[data-restaurant-open="1"] .truncated-name', divs => {
     return divs.map(div => div.innerHTML)
   })
   restaurants.forEach(console.log)
