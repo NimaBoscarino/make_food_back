7a1cf3f791f7d457e54293ac2d5bb0474a22b339
Mario Viens <marviens@gmail.com>
2019-04-04 10:37:51 -0700

    Move back to facebook login as regular sign in has changed.

--- api/index.js
+++ api/index.js
@@ -5,7 +5,9 @@ require('dotenv').config()
 
 const {FB_USER, FB_PASS, STD_USER, STD_PASS} = process.env
 
+const headless = false
 const city = 'vancouver'
+const waitUntilIdle = {waitUntil: 'networkidle0'}
 
 const facebookLogin = async page => {
   const FB_PAGE = {
@@ -13,6 +15,7 @@ const facebookLogin = async page => {
     passField: '#pass',
     loginButton: '#loginbutton',
   }
+  console.log('logging inna facebook')
   // Seems to wait til the entire fb page loads
   await page.goto('https://facebook.com', {
     waitUntil: 'networkidle2'
@@ -22,16 +25,30 @@ const facebookLogin = async page => {
   await page.type(FB_PAGE.passField, FB_PASS)
   await page.click(FB_PAGE.loginButton)
   await page.waitForNavigation()
+  console.log('logged inna facebook')
 }
 
 const skipTheDishesLoginFb = async page => {
   const STD_PAGE = {
-    facebookButton: '.btn-facebook',
+    loginButton: 'button:nth-child(3) div',
+    loginButton2: 'div > ul > div',
+    facebookButton: 'body > div > div > div > div > div > div > div:nth-child(1) > div > button',
+    facebookButton2: '#u_0_4 > div > div- > div > button',
   }
-  await page.goto('https://www.skipthedishes.com/user/login')
+  console.log('logging inna facebook sso on std')
+  await page.goto('https://www.skipthedishes.com/', waitUntilIdle)
+  await page.waitForSelector(STD_PAGE.loginButton)
+  await page.click(STD_PAGE.loginButton)
+  console.log('clicked login button')
+  await page.waitFor(1000)
+  await page.waitForSelector(STD_PAGE.loginButton2)
+  console.log('clicking second login button')
+  await page.click(STD_PAGE.loginButton2)
   await page.waitForSelector(STD_PAGE.facebookButton)
   await page.click(STD_PAGE.facebookButton)
+  console.log('logged inna facebook sso on std')
   await page.waitForNavigation()
+  await page.click(STD_PAGE.facebookButton2)
 }
 
 const skipTheDishesLogin = async page => {
@@ -69,23 +86,22 @@ const goToRestaurantPage = async page => {
 }
 
 ;(async () => {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({headless})
   const page = await browser.newPage()
 
   // Login to Facebook account
-  // await facebookLogin(page)
+  await facebookLogin(page)
 
   // Go to skip the dishes and login with facebook
-  // await skipTheDishesLoginFb(page)
+  await skipTheDishesLoginFb(page)
 
-  // Login with SkipTheDishes username/password
+  // Login with SkipTheDishes username/password - broken because their login page doesnt exist anymore!
   // await skipTheDishesLogin(page)
-  await skipTheDishesLogin(page)
-  
-  await page.waitFor(4000)
+  // await page.waitFor(4000)
 
   // Go to Vancouver SKD page and get a restaurant thats open
   await goToRestaurantPage(page)
+  await page.waitFor(4000)
 
   // Take a screenshot
   await page.screenshot({path: './screenshot.png'})
